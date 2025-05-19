a8af092d3b5871ed137e7d88965947c2a723af35
word1230 <2323864130@qq.com>
2025-05-19 09:32:39 +0800

    new project

---
+++ codeshowcommunity-backend/src/main/java/com/cheems/codeshowcommunitybackend/CodeshowcommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cheems.codeshowcommunitybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeshowcommunityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeshowcommunityBackendApplication.class, args);
+    }
+
+}
---
+++ codeshowcommunity-backend/src/test/java/com/cheems/codeshowcommunitybackend/CodeshowcommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheems.codeshowcommunitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeshowcommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
